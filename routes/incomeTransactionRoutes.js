c69e54e412effab20806839b27347460241febf9
Kuba <olo75jg12@gmail.com>
2022-03-29 20:49:59 +0200

    add crud operattions to incomeTransactionRoutes

--- routes/incomeTransactionRoutes.js
+++ routes/incomeTransactionRoutes.js
@@ -4,19 +4,56 @@ const requireLogin = require('../middlewares/requireLogin');
 const IncomeTransaction = mongoose.model('IncomeTransaction');
 
 module.exports = (app) => {
+  // create new income
   app.post('/api/createIncome', requireLogin, async (req, res) => {
-    const { date, amount, title, description, cattegory } = req.body;
+    try {
+      const { date, amount, title, description, cattegory } = req.body;
 
-    const incomeTransaction = new IncomeTransaction({
-      _user: req.user.id,
-      date,
-      amount,
-      title,
-      description,
-      cattegory
-    });
+      const incomeTransaction = new IncomeTransaction({
+        _user: req.user.id,
+        date,
+        amount,
+        title,
+        description,
+        cattegory
+      });
 
-    await incomeTransaction.save();
-    res.send(incomeTransaction);
+      await incomeTransaction.save();
+      res.send(incomeTransaction);
+    } catch (e) {
+      res.status(400).send({ error: e });
+    }
+  });
+
+  // get all incomes
+  app.get('/api/getAllIncome', requireLogin, async (req, res) => {
+    try {
+      const incomes = await IncomeTransaction.find({ _user: req.user.id });
+      res.send(incomes);
+    } catch (e) {
+      res.status(400).send({ error: e });
+    }
+  });
+
+  // get single income
+  app.get('/api/getIncome/:id', requireLogin, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const income = await IncomeTransaction.find({ _user: req.user.id, _id: id })
+      res.send(income);
+    } catch (e) {
+      res.status(400).send({ error: e });
+    }
+  });
+
+  // delete single income
+  app.delete('/api/deleteIncome/:id', requireLogin, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const income = await IncomeTransaction.findOneAndDelete({ _user: req.user.id, _id: id });
+      res.send(income);
+    } catch (e) {
+      res.status(400).send({ error: e });
+    }
   });
 };
