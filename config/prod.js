9d4e7b6f58bcc3f052425ec108010051c15b3fc4
Kuba <olo75jg12@gmail.com>
2022-05-03 16:29:54 +0200

    enabled proxy in googleOAuth

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,8 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 };
+
+ 
