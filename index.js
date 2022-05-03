cf6a78139b3830da5c1678db67e220ef56d9aa8c
Kuba <olo75jg12@gmail.com>
2022-05-03 17:19:25 +0200

    fixed path

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ require('./routes/expenseTransactionRoutes')(app);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
